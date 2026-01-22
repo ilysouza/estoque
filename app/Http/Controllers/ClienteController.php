e9af7f56e7c63fdd12f7d3fffc9b8e8bd75c924a
ilysouza <ily.souza@outlook.com>
2026-01-22 15:41:39 -0300

    Implementado CRUD de clientes

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    //
+
+    public function store(Request $request){
+    $verificacao = Cliente::where('cpf','=', $request)-> first();
+
+        $cliente = Cliente::create([
+            'nome'=> $request->nome,
+            'cpf'=> $request->cpf,
+            'idade'=> $request->idade
+        ]);
+
+        
+
+        if (!$verificacao) {
+        return response()->json($cliente);
+        }
+        else {
+            return response()->json('Este CPF já está sendo utilizado.');
+        }
+
+    }
+
+    public function index() {
+        $clientes = Cliente::all();
+
+        return response()->json($clientes);
+    }
+
+    public function show($id){
+        $cliente = Cliente::find($id);
+
+        if (!$id) {
+            return response()->json('Cliente não encontrado.');
+        }
+        return response()->json($cliente);
+    }
+
+    public function update($id, Request $request)
+    {
+        $verificacao = Cliente::where('cpf','=', $request)-> first();
+        $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json('Cliente não encontrado...');
+        }
+
+        if (isset($request->nome)) {
+            $cliente->nome = $request->nome;
+        }
+        if (isset($request->cpf)) {
+            $cliente->cpf = $request->cpf;
+        }
+        if (isset($request->idade)) {
+            $cliente->idade = $request->idade;
+        }
+
+        $cliente->update();
+
+        return response()->json($cliente);
+    }
+
+    public function delete($id)
+    {
+        $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json('Cliente não encontrado...');
+        }
+        $cliente->delete();
+
+        return response()->json('Cliente deletado com sucesso.');
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,12 @@ Route::get('/produto/{id}', [ProdutoController::class, 'show']); //Lista por id
 Route::put('/produto/{id}', [ProdutoController::class, 'update']); // atualiza
 
 Route::delete('/produto/{id}', [ProdutoController::class, 'delete']); // Deleta tal produto
+
+Route::post('/cliente', [ClienteController::class, 'store']);
+
+Route::get('/cliente', [ClienteController::class, 'index']); 
+Route::get('/cliente/{id}', [ClienteController::class, 'show']);
+
+Route::put('/cliente/{id}', [ClienteController::class, 'update']);
+
+Route::delete('/cliente/{id}', [ClienteController::class, 'delete']);
