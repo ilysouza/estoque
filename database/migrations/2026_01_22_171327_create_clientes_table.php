533f68aead105b110112bc40a0fbfd35ac1addcc
ilysouza <ily.souza@outlook.com>
2026-01-27 10:27:40 -0300

    Atualizando CRUD de Clientes

--- database/migrations/2026_01_22_171327_create_clientes_table.php
+++ database/migrations/2026_01_22_171327_create_clientes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 150);
-            $table->integer('cpf');
+            $table->string('cpf')->unique();
             $table->integer('idade');
             $table->timestamps();
         });
